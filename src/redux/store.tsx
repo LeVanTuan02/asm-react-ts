14f63a5b366696977abf5b096f52b350d8678337
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-04-05 18:53:21 +0700

    refactor: quản lý liên hệ - redux-toolkit

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -3,13 +3,15 @@ import sizeReducer from "./sizeSlice";
 import cateProductReducer from "./categoryProductSlice";
 import newsReducer from "./newsSlice";
 import cateNewsReducer from "./cateNewsSlice";
+import contactReducer from "./contactSlice";
 
 const store = configureStore({
     reducer: {
         size: sizeReducer,
         cateProduct: cateProductReducer,
         news: newsReducer,
-        cateNews: cateNewsReducer
+        cateNews: cateNewsReducer,
+        contact: contactReducer
     }
 })
 
